70d3f5553cce3b0b822f4759e0f44a24e27c5a94
SARAH-MIGADA-a <133857341+SARAH-MIGADA-a@users.noreply.github.com>
2024-05-23 18:09:42 +0300

    load balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# Update package information
+exec { 'apt-update':
+  command => '/usr/bin/apt-get -y update',
+}
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Create a basic HTML file
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Configure Nginx to add the custom HTTP header
+file_line { 'add custom header':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
+}
+
+# Ensure Nginx is running
+service { 'nginx':
+  ensure => running,
+}
