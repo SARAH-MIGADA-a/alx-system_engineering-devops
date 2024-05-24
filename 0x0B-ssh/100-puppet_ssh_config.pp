8796848e5dc80a38fe5220d22ede784cf53206ae
SARAH-MIGADA-a <133857341+SARAH-MIGADA-a@users.noreply.github.com>
2024-05-24 17:57:23 +0300

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# Using Puppet to change our server configuration files
+file_line { 'configure using private key ~/.ssh/school':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '  IdentityFile ~/.ssh/school',
+}
+
+# Disable password authentication
+file_line { 'authenticate using a password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '  PasswordAuthentication no',
+}
+
+file { '/etc/ssh/ssh_config':
+  ensure => present,
+}
